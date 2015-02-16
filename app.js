b9d96aa522722334540d46756a1ae87310e5c45a
Matt Rich <metropolis_4@hotmail.com>
2015-02-16 15:48:25 -0700

    Updates

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
-var indexController = require('./controllers/index.js');
+var fs = require('fs');
+
+fs.readFile('data.txt', function(err, data){
+    app.get('/', function(req, res) {
+        res.header('Content-Type', 'text/html');
+        res.send(data);
+    });
+});
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/', indexController.index);
+
 
 var server = app.listen(7848, function() {
 	console.log('Express server listening on port ' + server.address().port);
